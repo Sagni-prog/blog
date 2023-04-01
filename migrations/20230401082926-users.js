530ad67fd513121576b45847293e7bf7b325c59c
Sagni Alemayehu <sagnialemayehu69@gmail.com>
2023-04-01 11:36:58 +0300

    user migration

--- migrations/20230401082926-users.js
+++ migrations/20230401082926-users.js
@@ -1,14 +1,42 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
+  {import('sequelize-cli').Migration} 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    return queryInterface.createTable('Person',
+    {
+      id: {
+          type: Sequelize.DataTypes.INTEGER,
+          autoIncrement: true,
+          allowNull: false,
+          primaryKey: true
+      },
+      name: {
+         type: Sequelize.DataTypes.STRING,
+         allowNull: false
+      },
+     email: {
+         type: Sequelize.DataTypes.STRING,
+         allowNull: false,
+         unique: true
+      },
+      password: {
+         type: Sequelize.DataTypes.STRING,
+         allowNull: false
+      },
+      role: {
+         type: Sequelize.DataTypes.STRING,
+         deletedAt: 'user'
+      },
+      isActive: {
+         type: Sequelize.DataTypes.BOOLEAN,
+         defaultValue: false
+      },
+      passwordResetToken: DataTypes.STRING,
+      passwordResetTokenExpiresAt: Sequelize.DataTypes.DATE,
+      passworsChangedAt: Sequelize.DataTypes.DATE,
+      deletedAt: Sequelize.DataTypes.DATE
+ })
   },
 
   async down (queryInterface, Sequelize) {
